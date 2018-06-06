520c3f4e059e5075776f5de50f2d722d1bd8785b
XatMassacrE <xxat.massacre@gmail.com>
2018-06-06 21:43:31 +0800

    Add deploy info

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-const Champion = artifacts.require('Champion.sol')
+const ChampionSimple = artifacts.require('ChampionSimple.sol')
+const ChampionRound = artifacts.require('ChampionRound.sol')
 
 module.exports = function(deployer) {
-  deployer.deploy(Champion)
+  const startTime = 1528988400
+  deployer.deploy(ChampionSimple, startTime, 5*10**16)
+  deployer.deploy(ChampionRound, startTime, 5*10**16)
 }
