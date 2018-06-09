00b329ee4b745400daa0bff62886be52a3abd865
XatMassacrE <xxat.massacre@gmail.com>
2018-06-09 15:51:27 +0800

    Update migration files to comment out ChampionRound

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,5 @@ const ChampionRound = artifacts.require('ChampionRound.sol')
 module.exports = function(deployer) {
   const startTime = 1528988400
   deployer.deploy(ChampionSimple, startTime, 5*10**16)
-  deployer.deploy(ChampionRound, startTime, 5*10**16)
+  //deployer.deploy(ChampionRound, startTime, 5*10**16)
 }
