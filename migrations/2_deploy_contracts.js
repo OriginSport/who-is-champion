c0776b7264c99477116570f72bfec478f37c8969
XatMassacrE <xxat.massacre@gmail.com>
2018-06-13 11:04:17 +0800

    Update startTime to second round

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
 const ChampionSimple = artifacts.require('ChampionSimple.sol')
-const ChampionRound = artifacts.require('ChampionRound.sol')
 
 module.exports = function(deployer) {
-  const startTime = 1528988400
-  deployer.deploy(ChampionSimple, startTime, 5*10**16)
-  //deployer.deploy(ChampionRound, startTime, 5*10**16)
+  const startTime = 1529431200
+  const minimumBet = 5*10**16
+  deployer.deploy(ChampionSimple, startTime, minimumBet)
 }
