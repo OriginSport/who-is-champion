ea87aeea93d96717c49a90b5426d71b4a1a286a9
XatMassacrE <xxat.massacre@gmail.com>
2018-06-17 20:40:49 +0800

    Update startTime

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
 const ChampionSimple = artifacts.require('ChampionSimple.sol')
 
 module.exports = function(deployer) {
-  const startTime = 1529431200
+  //const startTime = 1529431200
+  const startTime = 1530367200
   const minimumBet = 5*10**16
   deployer.deploy(ChampionSimple, startTime, minimumBet)
 }
